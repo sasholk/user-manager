fd1da98fc61f15840c36d6b613c8444dc3e103bc
sasholk <iccc97@gmail.com>
2025-03-19 21:19:06 +0200

    feat: initialize project

---
+++ src/app/App.tsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        User Manager
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          User Manger App
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: '@img',
+        replacement: path.resolve(__dirname, './public/img'),
+      },
+    ],
+  },
+});
